c2313124d119058d42542aed9afc9368e2a759b3
↝Élise☀︎ <eliseduverdier@gmail.com>
2021-07-10 22:29:32 +0200

    feat(layout): more route and layouts

--- app/routes/translate.js
+++ app/routes/translate.js
@@ -5,17 +5,23 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
     res.render('translate/input');
-  });
-
-/* TRANSLATE from english to solresol. */
-router.get('/en-srs/:word', function(req, res, next) {
-    //console.log(req)
-  res.render('translate/en-srs', { word: req.params.word });
 });
 
+/* TRANSLATE from english to solresol. */
+router.get('/en-srs', function(req, res, next) {
+    res.render('translate/en-srs', { word: req.query.word });
+});
+router.get('/en-srs/:word', function(req, res, next) {
+    res.render('translate/en-srs', { word: req.params.word });
+});
+
+
 /* TRANSLATE from solresol to english. */
+router.get('/srs-en', function(req, res, next) {
+    res.render('translate/srs-en', { word: req.params.word });
+});
 router.get('/srs-en/:word', function(req, res, next) {
-  res.render('translate/srs-en', { word: req.params.word });
+    res.render('translate/srs-en', { word: req.params.word });
 });
 
 module.exports = router;
